20f552b7b6d652fe662c58e925a75fc1acbe11e1
abskaushal <abskaushal@gmail.com>
2016-06-06 23:52:21 +0530

    Push Add node at head of linked list

---
+++ DataStructures/Linked List/insert_node_at_head/Solution.java
@@ -0,0 +1,25 @@
+/*
+  Insert Node at the beginning of a linked list 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+// This is a "method-only" submission. 
+// You only need to complete this method. 
+
+Node Insert(Node head,int x) {
+    if(head == null){
+        head.data = x;
+        head.next = null;
+    }else{
+        Node n = new Node();
+        n.data = x;
+        n.next = head;
+        head = n;
+    }
+    
+    return head;
+}
